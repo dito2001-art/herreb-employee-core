c8e4762ed9c231801b01a477d3de468aff488517
dito2001-art <dito2001@gmail.com>
2026-09-15 20:12:15 -0300

    feat(emp001): export persistent sales state

--- src/core/index.ts
+++ src/core/index.ts
@@ -13,6 +13,7 @@ export * from "./proactive-sales";
 export * from "./sales-loop";
 export * from "./sales-loop-executor";
 export * from "./sales-state";
+export * from "./sales-state-persistent";
 export * from "./sales-cycle";
 export * from "./adapters";
 export * from "./executor";
