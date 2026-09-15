2d6890f54e1f98bdb59856b28ad27133a09fa870
dito2001-art <dito2001@gmail.com>
2026-09-15 10:49:41 -0300

    feat(emp003): export marketing brain contracts

--- src/core/index.ts
+++ src/core/index.ts
@@ -7,6 +7,7 @@ export * from "./audit";
 export * from "./tenant";
 export * from "./tenant-manifest";
 export * from "./knowledge";
+export * from "./marketing";
 export * from "./adapters";
 export * from "./executor";
 export * from "./durable";
