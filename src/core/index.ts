61de113ffc9949f6ace6cbb7a20f0d9f286fb166
dito2001-art <dito2001@gmail.com>
2026-09-15 17:35:46 -0300

    feat(emp003): export repository-backed marketing brain

--- src/core/index.ts
+++ src/core/index.ts
@@ -11,6 +11,7 @@ export * from "./knowledge-repository";
 export * from "./knowledge-onboarding";
 export * from "./marketing";
 export * from "./marketing-brain";
+export * from "./marketing-brain-repository";
 export * from "./adapters";
 export * from "./executor";
 export * from "./durable";
